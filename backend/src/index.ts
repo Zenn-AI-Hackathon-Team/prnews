11b28935833f1e1de98675e7e610b32748e407fc
masa-massara <124395897+masa-massara@users.noreply.github.com>
2025-06-30 21:02:45 +0900

    fix: explicitly set hostname to 0.0.0.0 in serve

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -104,9 +104,10 @@ async function main() {
 		{
 			fetch: app.fetch,
 			port: port,
+			hostname: "0.0.0.0",
 		},
 		(info) => {
-			console.log(`Server is running on http://localhost:${info.port}`);
+			console.log(`Server is running on http://${info.address}:${info.port}`);
 		},
 	);
 
